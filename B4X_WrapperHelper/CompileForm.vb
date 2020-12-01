dab735af5c3a688e84a1d0cebdee0c72fef1f685
NIMO <laomms@gmail.com>
2020-12-01 20:38:31 +0700

    modify

--- B4X_WrapperHelper/CompileForm.vb
+++ B4X_WrapperHelper/CompileForm.vb
@@ -64,7 +64,7 @@ Public Class CompileForm
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         RichTextBox1.Text = ""
-        Dim java As String = ""
+        Dim javafiles As String = ""
         Dim cp As String = ""
         If ProjectPath = "" Or ProjectPath.Contains("\") = False Then Return
         If B4AShared = "" Or Core = "" Or androidjarPath = "" Then Return
@@ -100,17 +100,17 @@ Public Class CompileForm
         End If
         Dim javaList = Directory.GetFiles(ProjectPath, "*.*", SearchOption.AllDirectories).Where(Function(f) New List(Of String) From {".java"}.IndexOf(Path.GetExtension(f)) >= 0).ToArray()  '{".kt", ".java"}
         If javaList.Count > 0 Then
-            java = String.Join(" ", javaList).Replace(ProjectPath + "\", "").Replace("\", "/")
-            'Debug.Print(java)
+            javafiles = String.Join(" ", javaList).Replace(ProjectPath + "\", "").Replace("\", "/")
         End If
         Dim javac = SysEnvironment.GetSysEnvironmentByName("JAVA_HOME") + "\bin\javac"
+
 
         Using p1 As New Process
             p1.StartInfo.CreateNoWindow = True
             p1.StartInfo.Verb = "runas"
             p1.StartInfo.FileName = "cmd.exe"
             p1.StartInfo.WorkingDirectory = ProjectPath
-            p1.StartInfo.Arguments = String.Format(" /c {0} -Xmaxerrs 1 -nowarn -Xlint:none -J-Xmx512m  -version -encoding UTF-8 -d {1} -cp {2} {3}  2>>&1", javac, "bin/classes", cp, java)
+            p1.StartInfo.Arguments = String.Format(" /c {0} -Xmaxerrs 1 -nowarn -Xlint:none -J-Xmx512m  -version -encoding UTF-8 -d {1} -cp {2} {3}  2>>&1", javac, "bin/classes", cp, javafiles)
             p1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
             p1.StartInfo.UseShellExecute = False
             p1.StartInfo.RedirectStandardOutput = True
@@ -143,7 +143,7 @@ Public Class CompileForm
                 p1.StartInfo.Verb = "runas"
                 p1.StartInfo.FileName = "cmd.exe"
                 p1.StartInfo.WorkingDirectory = ProjectPath
-                p1.StartInfo.Arguments = String.Format(" /c {0} -doclet BADoclet -docletpath {1} -classpath {2} -ProjectPath {3} -b4atarget {4} -b4aignore b4a.java.library.first {5}  2>>&1", javadoc, My.Computer.FileSystem.SpecialDirectories.Temp + "\B4X", cp, "src", Path.GetDirectoryName(ProjectPath) + "\" + Path.GetFileName(ProjectPath) + ".xml", java)
+                p1.StartInfo.Arguments = String.Format(" /c {0} -doclet BADoclet -docletpath {1} -classpath {2} -ProjectPath {3} -b4atarget {4} -b4aignore org,com.android,com.example,com.hoho {5}  2>>&1", javadoc, My.Computer.FileSystem.SpecialDirectories.Temp + "\B4X", cp, "src", Path.GetDirectoryName(ProjectPath) + "\" + Path.GetFileName(ProjectPath) + ".xml", javafiles)
                 p1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
                 p1.StartInfo.UseShellExecute = False
                 p1.StartInfo.RedirectStandardOutput = True
@@ -155,6 +155,9 @@ Public Class CompileForm
                     RichTextBox1.AppendText(vbNewLine + output)
                 End Using
             End Using
+            RichTextBox1.AppendText("Compilation is complete")
+            RichTextBox1.SelectionStart = RichTextBox1.Text.Length
+            RichTextBox1.ScrollToCaret()
         End If
 
     End Sub
